3086a18cf309aec146ae51c0d9613d7853042a38
lbustillo12 <lizy.bustillo@unah.hn>
2024-07-12 01:07:38 -0600

    Proyecto de calculadora basica.

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Lizy Bustillo
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Pantalla ventana = new Pantalla(); 
+            ventana.setVisible(true);
+        
+    }
+    
+}
